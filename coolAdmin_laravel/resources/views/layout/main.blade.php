3dd941344ed1c2065f60699f20fc69ab8db8fdd9
Ali Asghar <it.dev81@dawateislami.net>
2023-07-24 17:26:11 +0500

    laravel intigration and changes

---
+++ coolAdmin_laravel/resources/views/layout/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title> @yield('title')</title>
+    @include('layout.script')
+</head>
+
+@include('layout.menu-bar')
+@yield('content')
+@include('layout.footer')
---
+++ coolAdmin_laravel/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () { return view('welcome'); });
+Route::get('/index', function () { return view('index'); });
+Route::get('/index2', function () { return view('index2'); });
+Route::get('/index3', function () { return view('index3'); });
+Route::get('/index4', function () { return view('index4'); });
+Route::get('/chart', function () { return view('chart'); });
+Route::get('/table', function () { return view('table'); });
+Route::get('/form', function () { return view('form'); });
+Route::get('/calendar', function () { return view('calendar'); });
+Route::get('/map', function () { return view('map'); });
+Route::get('/login', function () { return view('login'); });
+Route::get('/register', function () { return view('register'); });
+Route::get('/forget-pass', function () { return view('forget-pass'); });
+Route::get('/button', function () { return view('button'); });
+Route::get('/badge', function () { return view('badge'); });
+Route::get('/tab', function () { return view('tab'); });
+Route::get('/card', function () { return view('card'); });
+Route::get('/alert', function () { return view('alert'); });
+Route::get('/progress-bar', function () { return view('progress-bar'); });
+Route::get('/modal', function () { return view('modal'); });
+Route::get('/switch', function () { return view('switch'); });
+Route::get('/grid', function () { return view('grid'); });
+Route::get('/fontawesome', function () { return view('fontawesome'); });
+Route::get('/typo', function () { return view('typo'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
+Route::get('/', function () { return view('welcome'); });
